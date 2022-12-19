a4f1879e6c432bb3b9fef9a5bbf9e16854c59e8c
Chathuranga97lj <chathuit97@gmail.com>
2022-12-19 14:46:47 +0530

    chage app bg color

---
+++ lib/home_new.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.lightGreenAccent,
+    );
+  }
+}
